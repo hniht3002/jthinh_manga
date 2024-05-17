8dcd9e35fe886b817c146e451e8e999af1ec655b
hniht3002 <s2okaza2s@gmail.com>
2024-05-17 10:19:32 +0700

    set up tailwind

--- src/Screen/Home.tsx
+++ src/Screen/Home.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text } from "react-native";
 import CustomText from "../CustomComponent/CustomText/CustomText";
-
+import tw from 'twrnc';
 function Home() {
     return ( <>
-        <CustomText align="center">Home</CustomText>
+        <CustomText align="center" style={tw`text-rose-400`}>Home</CustomText>
     </> );
 }
 
